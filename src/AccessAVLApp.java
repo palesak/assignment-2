369ed8c032515de2cb7f2ae1282e26726628b4b3
Palesa Khoali <palesakhoali@Setups-MBP.home>
2021-04-26 11:39:02 +0200

    Works when Printing the entire tree, works when adding values but only up until we find the value, the exits

--- src/AccessAVLApp.java
+++ src/AccessAVLApp.java
@@ -1,301 +1,397 @@
-import java.io.BufferedOutputStream;
-import java.io.BufferedWriter;
-import java.io.EOFException;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.io.PrintWriter;
-import java.io.Serializable;
+// Hussein's AVL Tree
+// 2 April2017
+// Hussein Suleman
+import java.io.*;
+import java.util.*;
 
-import java.util.Scanner;
-import java.util.StringTokenizer;
-import java.util.Vector;
 
-/**
- *
- * @author Palesa Khoali
- */
+class AccessAVLApp {
+    public static class BinaryTreeNode<String> {
+        String data;
+        BinaryTreeNode<String> left;
+        BinaryTreeNode<String> right;
+        int height;
 
-class AccessAVLApp{
-public static class Students implements Comparable<Students>, Serializable {
-    private static final long serialVersionUID = 1L;
+        public BinaryTreeNode(String d, BinaryTreeNode<String> l, BinaryTreeNode<String> r) {
+            data = d;
+            left = l;
+            right = r;
+            height = 0;
+        }
 
-    private int ID;
-    private String fName;
-    String lName;
-    public Students getFistName;
+        BinaryTreeNode<String> getLeft() {
+            return left;
+        }
 
-    Students(char std_ID, String std_fName, String std_lName){
-        ID = std_ID;
-        fName = std_fName;
-        lName = std_lName;
-
-    }
-    public void print(){
-        System.out.println(ID);
-        System.out.println(fName);
-        System.out.println(lName);
-
-    }
-    @Override
-    public String toString(){
-        return ID + "-" + fName + "-" + lName;
-    }
-    public int getID()
-    {
-        return ID;
-    }
-    public void setID (int std_ID)
-    {
-        ID = std_ID;
-    }
-    public String getfName()
-    {
-        return fName;
-    }
-    public void setfName (String std_fName)
-    {
-        fName = std_fName;
-    }
-    public String getlName()
-    {
-        return lName;
-    }
-    public void setlName (String std_lName)
-    {
-        lName = std_lName;
-    }
-    @Override
-    public int compareTo(Students o) {
-        return 0;
+        BinaryTreeNode<String> getRight() {
+            return right;
+        }
     }
 
-    public void input(){
-        System.out.print("Please input Students \n");
-        Scanner myScanner = new Scanner(System.in);
-        System.out.println("Please input a Students ID");
-        ID = myScanner.nextInt();
-        myScanner.nextLine();
-        System.out.println("Please input Students fName");
-        fName = myScanner.nextLine();
-        System.out.println("Please input StudentslName");
-        lName = myScanner.nextLine();
+    public static class BinaryTree<String> {
+        BinaryTreeNode<String> root;
 
-    }
+        public BinaryTree() {
+            root = null;
+        }
 
-}
-static class AVLNode
-{
-    AVLNode left, right;
-    char ID;
-    /*@FindBy(css="")
-    private WebElement webElement;
-    @FindBy(css="")
-    private WebElement webElement;
-    @FindBy(css="")
-    private WebElement webElement;*/
-    String fName;
-    String lName;
+        public int getHeight() {
+            return getHeight(root);
+        }
 
-    public AVLNode()
-    {
-        left = null;
-        right = null;
-        ID = 0;
-        fName = null;
-    }
-    public AVLNode(char n)
-    {
-        left = null;
-        right = null;
-        ID = n;
-        fName = null;
-    }
-}
-static class AVLTree
-{
-    private static AVLNode root;
-
-    public AVLTree()
-    {
-        root = null;
-    }
-
-    public boolean isEmpty()
-    {
-        return root==null;
-    }
-    public void makeEmpty()
-    {
-        root = null;
-    }
-    private int ID(AVLNode t)
-    {
-        return t == null ? -1 : t.ID;
-    }
-    private int max(int lhs, int rhs)
-    {
-        return lhs > rhs ? lhs : rhs;
-    }
-    private AVLNode insert(AVLNode t, char ID)
-    {
-        if(t == null)
-            t = new AVLNode(ID);
-        if (ID < t.ID)
-            t.left = insert(t.left, ID);
-        else {
-            if(ID > t.ID)
-                t.right = insert(t.right, ID);
-
+        public int getHeight(BinaryTreeNode<String> node) {
+            if (node == null)
+                return -1;
             else
-
-                return t;
+                return 1 + Math.max(getHeight(node.getLeft()), getHeight(node.getRight()));
         }
 
-
-        return t;
-
-    }
-    public void inorder()
-    {
-        inorder(root);
-    }
-    public void inorder(AVLNode r)
-    {
-        if(r != null)
-        {
-            inorder(r.left);
-            System.out.print(r.ID +" ");
-            inorder(r.right);
-
+        public int getSize() {
+            return getSize(root);
         }
-    }
-    public void preorder()
-    {
-        preorder(root);
-    }
-    private void preorder(AVLNode r)
-    {
-        if(r !=null)
-        {
-            System.out.print(r.ID +" ");
-            preorder(r.left);
-            preorder(r.right);
 
+        public int getSize(BinaryTreeNode<String> node) {
+            if (node == null)
+                return 0;
+            else
+                return 1 + getSize(node.getLeft()) + getSize(node.getRight());
         }
-    }
-    public void postorder()
-    {
-        postorder(root);
-    }
 
-    private void postorder(AVLNode r)
-    {
-        if(r != null)
-        {
-            postorder(r.left);
-            postorder(r.right);
-            System.out.print(r.ID +" ");
+        public void visit(BinaryTreeNode<String> node) {
+            System.out.println(node.data);
         }
-    }
-    /*public static int Read() {
-        int count = 0;
-        try {
-            Vector<Students> vector = new Vector<Students>();
-            FileInputStream saveFile = new FileInputStream("../oklist.txt");
-            ObjectInputStream save;
-            try {
-                for(;;){
-                    save = new ObjectInputStream(saveFile);
-                    Students std = (Students) save.readObject();
-                    vector.add(std);
-                    count++;
-                }
-            }catch(EOFException e){
 
+        public void preOrder() {
+            preOrder(root);
+        }
+
+        public void preOrder(BinaryTreeNode<String> node) {
+            if (node != null) {
+                visit(node);
+                preOrder(node.getLeft());
+                preOrder(node.getRight());
             }
-            saveFile.close();
-        }catch(Exception exc){
-            exc.printStackTrace();
         }
-        return count;
-    }*/
-    public void Write(Students stds) {
-        try
-        {
-            FileOutputStream fileOut = new FileOutputStream("../oklist,txt", true);
-            ObjectOutputStream out = new ObjectOutputStream(new BufferedOutputStream(fileOut));
 
-            out.writeObject(stds);
-            out.close();
-            fileOut.close();
-            System.out.println("Serialized data is saved in info.ser");
-        }catch(IOException i)
-        {
+        public void postOrder() {
+            postOrder(root);
+        }
 
+        public void postOrder(BinaryTreeNode<String> node) {
+            if (node != null) {
+                postOrder(node.getLeft());
+                postOrder(node.getRight());
+                visit(node);
+            }
+        }
+
+        public void inOrder() {
+            inOrder(root);
+        }
+
+        public void inOrder(BinaryTreeNode<String> node) {
+            if (node != null) {
+                inOrder(node.getLeft());
+                visit(node);
+                inOrder(node.getRight());
+            }
+        }
+
+        public void levelOrder() {
+            if (root == null)
+                return;
+            BTQueue<String> q = new BTQueue<String>();
+            q.enQueue(root);
+            BinaryTreeNode<String> node;
+            while ((node = q.getNext()) != null) {
+                visit(node);
+                if (node.getLeft() != null)
+                    q.enQueue(node.getLeft());
+                if (node.getRight() != null)
+                    q.enQueue(node.getRight());
+            }
         }
     }
 
-}
+    public static class BTQueueNode<String> {
+        BinaryTreeNode<String> node;
+        BTQueueNode<String> next;
 
-    public static void main(String[] args)
-    {
-        Students std = null;
+        public BTQueueNode(BinaryTreeNode<String> n, BTQueueNode<String> nxt) {
+            node = n;
+            next = nxt;
+        }
+    }
 
-        char ID = 0;
+    public static class BTQueue<String> {
+        BTQueueNode<String> head;
+        BTQueueNode<String> tail;
 
-        String fName = null;
-        //char ID = 0;
-        String lName = null;
-        try (Scanner scan = new Scanner(System.in)) {
-
-            AVLTree avl = new AVLTree();
-
-            System.out.println("Binary Search Tree Operations \n");
-            char ch;
-
-            do {
-                System.out.println("\n Binary Search Tree operations\n");
-                System.out.println("1. insert ");
-                Scanner input = new Scanner(System.in);
-    //Int choice = scan.nextLine();
-                if (input.nextLine() != "") {
-                    //case "test 1":
-                        System.out.println("Please input Students \n");
-
-                        System.out.println("Please input a student ID");
-                        ID = input.next().charAt(0);
-                        scan.nextLine();
-                        System.out.println("Please input Students fName");
-                        fName = scan.nextLine();
-                        System.out.println("Please input Students lName");
-                        lName = scan.nextLine();
-                        std = new Students(ID, fName, lName);
-                        //bst.insert(std);
-                        avl.Write(std);
-                        break;
-                    /*default:
-                        System.out.println("Access Denied! \n");
-                        break;*/
-                }
-                System.out.print("\nTest");
-                ch = scan.next().charAt(0);
-                /*System.out.print("\nPost order : ");
-                avl.postorder();
-                System.out.print("\nPre order : ");
-                avl.preorder();
-                System.out.print("\nIn order : ");
-                avl.inorder();
-
-                System.out.println("\nDo you want to continue (Type y or n) \n");
-                ch = scan.next().charAt(0);*/
-
-            } while (ch == 'Y' || ch == 'y');
+        public BTQueue() {
+            head = null;
+            tail = null;
         }
 
+        public BinaryTreeNode<String> getNext() {
+            if (head == null)
+                return null;
+            BTQueueNode<String> qnode = head;
+            head = head.next;
+            if (head == null)
+                tail = null;
+            return qnode.node;
+        }
+
+        public void enQueue(BinaryTreeNode<String> node) {
+            if (tail == null) {
+                tail = new BTQueueNode<String>(node, null);
+                head = tail;
+            } else {
+                tail.next = new BTQueueNode<String>(node, null);
+                tail = tail.next;
+            }
+        }
+    }
+
+    public static class AVLTree<String extends Comparable<? super String>> extends BinaryTree<String> {
+        public int height(BinaryTreeNode<String> node) {
+            if (node != null)
+                return node.height;
+            return -1;
+        }
+
+        public int balanceFactor(BinaryTreeNode<String> node) {
+            return height(node.right) - height(node.left);
+        }
+
+        public void fixHeight(BinaryTreeNode<String> node) {
+            node.height = Math.max(height(node.left), height(node.right)) + 1;
+        }
+
+        public BinaryTreeNode<String> rotateRight(BinaryTreeNode<String> p) {
+            BinaryTreeNode<String> q = p.left;
+            p.left = q.right;
+            q.right = p;
+            fixHeight(p);
+            fixHeight(q);
+            return q;
+        }
+
+        public BinaryTreeNode<String> rotateLeft(BinaryTreeNode<String> q) {
+            BinaryTreeNode<String> p = q.right;
+            q.right = p.left;
+            p.left = q;
+            fixHeight(q);
+            fixHeight(p);
+            return p;
+        }
+
+        public BinaryTreeNode<String> balance(BinaryTreeNode<String> p) {
+            fixHeight(p);
+            if (balanceFactor(p) == 2) {
+                if (balanceFactor(p.right) < 0)
+                    p.right = rotateRight(p.right);
+                return rotateLeft(p);
+            }
+            if (balanceFactor(p) == -2) {
+                if (balanceFactor(p.left) > 0)
+                    p.left = rotateLeft(p.left);
+                return rotateRight(p);
+            }
+            return p;
+        }
+
+        public void insert(String d) {
+            root = insert(d, root);
+        }
+
+        public BinaryTreeNode<String> insert(String d, BinaryTreeNode<String> node) {
+            if (node == null)
+                return new BinaryTreeNode<String>(d, null, null);
+            if (d.compareTo(node.data) <= 0)
+                node.left = insert(d, node.left);
+            else
+                node.right = insert(d, node.right);
+            return balance(node);
+        }
+
+        public void delete(String d) {
+            root = delete(d, root);
+        }
+
+        public BinaryTreeNode<String> delete(String d, BinaryTreeNode<String> node) {
+            if (node == null) return null;
+            if (d.compareTo(node.data) < 0)
+                node.left = delete(d, node.left);
+            else if (d.compareTo(node.data) > 0)
+                node.right = delete(d, node.right);
+            else {
+                BinaryTreeNode<String> q = node.left;
+                BinaryTreeNode<String> r = node.right;
+                if (r == null)
+                    return q;
+                BinaryTreeNode<String> min = findMin(r);
+                min.right = removeMin(r);
+                min.left = q;
+                return balance(min);
+            }
+            return balance(node);
+        }
+
+        public BinaryTreeNode<String> findMin(BinaryTreeNode<String> node) {
+            if (node.left != null)
+                return findMin(node.left);
+            else
+                return node;
+        }
+
+        public BinaryTreeNode<String> removeMin(BinaryTreeNode<String> node) {
+            if (node.left == null)
+                return node.right;
+            node.left = removeMin(node.left);
+            return balance(node);
+        }
+
+        public BinaryTreeNode<String> find(String d) {
+            if (root == null)
+                return null;
+            else
+                return find(d, root);
+        }
+
+        public BinaryTreeNode<String> find(String d, BinaryTreeNode<String> node) {
+            if (d.compareTo(node.data) == 0)
+                return node;
+            else if (d.compareTo(node.data) < 0)
+                return (node.left == null) ? null : find(d, node.left);
+            else
+                return (node.right == null) ? null : find(d, node.right);
+        }
+
+        public void treeOrder() {
+            treeOrder(root, 0);
+        }
+
+        public void treeOrder(BinaryTreeNode<String> node, int level) {
+            if (node != null) {
+                for (int i = 0; i < level; i++)
+                    System.out.print(" ");
+                System.out.println(node.data);
+                treeOrder(node.left, level + 1);
+                treeOrder(node.right, level + 1);
+            }
+        }
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        AVLTree<String> bt = new AVLTree<String>();
+
+        // reading the file and keeping an buffer to tell me what my position is
+        FileReader read = new FileReader("../data/oklist.txt");
+        BufferedReader buff = new BufferedReader(read);
+
+        // To store each line as one string
+
+
+/*
+      // test for single node
+      bt.root = new BinaryTreeNode<Integer> (10, null, null);
+*/
+
+/*
+      // test for 2-level tree
+      bt.root = new BinaryTreeNode<Integer> (
+          1,
+          new BinaryTreeNode<Integer> (2, null, null),
+          new BinaryTreeNode<Integer> (3, null, null)
+      );
+*/
+
+
+        // test for 3-level tree
+        if (args.length == 0) {
+            String line;
+            while ((line = buff.readLine()) != null) {
+                bt.insert(line);
+                System.out.println(line);
+            }
+        } else {
+            String line;
+            String[] word;// = new String[];
+            word = args;
+
+
+            //String[] wordChecker = new String[];
+            //String[] line = new String[];
+            //wordChecker = line.split(" ", 0);
+
+            while ((line = buff.readLine()) != null) {
+                String letters = "";
+                String[] wordChecker;
+                wordChecker = line.split(" ", 0);
+                letters = wordChecker[0];
+                bt.insert(letters);
+                String others = "";
+                if (bt.find(word[0]) != null) {
+
+                    others = wordChecker[1] + " " + wordChecker[2];
+                    System.out.println(others);
+                    break;
+                }
+         /*if (bt.find(word[0]) != null)
+         {
+            //System.out.println("yes");
+            //System.out.println(word[1] +" "+word[2]);
+         }*/
+            }
+            //System.out.println(others);
+      /*bt.insert ("xeno");
+      bt.insert ("cat");
+      bt.insert ("fat");
+      bt.insert ("pat");
+      bt.insert ("mat");
+      bt.insert ("quar");
+      bt.insert ("rat");
+      bt.insert ("uno");*/
+
+/*
+                   5
+            1             7
+                2      6     9
+                  4        8
+                 3
+*/
+            //System.out.println ("Inorder : ");
+            //bt.treeOrder ();
+
+      /*System.out.println ("Search 2 : ");
+      System.out.println (bt.find (2));
+      System.out.println ("Search 7 : ");
+      System.out.println (bt.find (7));
+      System.out.println ("Search 5 : ");
+      System.out.println (bt.find (5));
+      System.out.println ("Search 3 : ");
+      System.out.println (bt.find (3));
+      System.out.println ("Search 10 : ");
+      System.out.println (bt.find (10));
+
+      System.out.println ("Delete 2 : ");
+      bt.delete (9);
+      bt.treeOrder ();
+      System.out.println ("Delete 7 : ");
+      bt.delete (6);
+      bt.treeOrder ();
+      System.out.println ("Delete 5 : ");
+      bt.delete (7);
+      bt.treeOrder ();
+      System.out.println ("Delete 3 : ");
+      bt.delete (8);
+      bt.treeOrder ();
+      System.out.println ("Delete 10 : ");
+      bt.delete (5);*/
+            //bt.treeOrder ();
+        }
     }
 }
 
